c7dd8ae64b7fd2e72d466e40a847adbcf1d4b99e
Chonchol <nafisislam161@gmail.com>
2023-06-04 12:15:35 +0600

    Install vue 3

---
+++ bitfumes-tutorial/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul>
+    <li v-for="(hero , index) in dcHeros" :key="index"> 
+     {{ index }} : {{  hero.name }}
+    </li>
+  </ul>
+  <input :value = "newHero" />
+</template>
+
+<script>
+export default 
+{
+  data(){
+    return {
+      newHero: "Aquaman",
+      dcHeros:
+      [
+        { name: "superman"  },
+        { name: "Flash"  },
+        { name: "Arrow" }
+      ]
+    }
+  }
+}
+</script>
