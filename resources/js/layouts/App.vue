d0d092d0d505b825ea7da8179335db617af3ab6e
Chonchol <nafisislam161@gmail.com>
2023-06-06 17:48:58 +0600

    update web

--- resources/js/layouts/App.vue
+++ resources/js/layouts/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div>Hello laravel 3 Nafisss</div>
-        <router-link to="/">Home</router-link>
+        <router-link :to="{name: 'Home'}">Home</router-link>
         <router-link to="/login">Login</router-link>
         <router-link to="/register">Register</router-link>
         <router-view></router-view>
